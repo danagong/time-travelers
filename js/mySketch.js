a080ae5b95c6ce21ecb01443104f160d5aa8a7e3
Dana Gong <dg588@cornell.edu>
2020-10-11 19:20:18 -0400

    dupate

--- js/mySketch.js
+++ js/mySketch.js
@@ -1,6 +1,7 @@
 //p5.js shader basic structure ref from https://www.openprocessing.org/sketch/920144
 //mySketch.js is a modified version of sketch954948 from openprocessing.org
 
+
 var canvas1 = function(p){
 
 
@@ -31,7 +32,7 @@ p.draw = function() {
 	theShader.setUniform('tex0',drawingGraphics)
 	
 	drawingGraphics.clear(0,0,p.width,p.height)
-	// drawingGraphics.ellipse(mouseX,mouseY,850)
+	//drawingGraphics.ellipse(mouseX,mouseY,850)
 	
 	drawingGraphics.stroke(255)
 	drawingGraphics.strokeWeight(2)
@@ -44,7 +45,7 @@ p.draw = function() {
 		
 		// drawingGraphics.noFill()
 	for(var i=00;i<800;i+=200){
-		drawingGraphics.ellipse(mouseX,mouseY,i)
+		drawingGraphics.ellipse(p.mouseX,p.mouseY,i)
 	}
 	drawingGraphics.pop()
 	
@@ -59,10 +60,8 @@ p.draw = function() {
 		}
 
 		for(var i=0;i<p.height+400;i+=200){
-			if(i<600){
-				var word= drawingGraphics.text("TIME TRAVELERS",50,i+(p.frameCount/(5/p.log(mouseY))  )%200  )
-			}
-		
+		 drawingGraphics.text("TIME TRAVELERS",50,i+(p.frameCount/(5/p.log(mouseY))  )%200  )
+			
 
 			
 		}

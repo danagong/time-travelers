65ef0f39603908df856d41a606feb2cad7c752a7
Dana Gong <dg588@cornell.edu>
2020-12-05 15:28:56 -0500

    submission

--- js/mySketch.js
+++ js/mySketch.js
@@ -19,7 +19,7 @@ p.setup = function() {
 	p.noStroke();
 	WebglGraphics = p.createGraphics(p.width,p.height,p.WEBGL)
 	drawingGraphics = p.createGraphics(p.width,p.height)
-	p.background(255)
+	p.background('rgb(25,25,25)')
 	mouseX = p.width/2
 	mouseY = p.height/2
 }

0732209ce4d063850d2b6c536a195786000b9312
Dana Gong <dg588@cornell.edu>
2021-01-08 20:10:27 -0500

    update

--- js/bubbles.js
+++ js/bubbles.js
@@ -4,13 +4,31 @@ let _maxPoint;
 let _aryObject = [];
 let _objectNum;
 
+let theShader;
+
+p.preload = function(){
+	theShader = new p5.Shader(this.renderer,vert,frag)
+}
+
+let drawingGraphics
+let WebglGraphics
+
 p.setup = function()  {
     p.createCanvas(p.windowWidth,800);
   p.frameRate(30);
   p.colorMode(p.HSB, 255,255,10,10);
+
+  p.noStroke();
+	WebglGraphics = p.createGraphics(p.width,p.height,p.WEBGL)
+	drawingGraphics = p.createGraphics(p.width,p.height)
+	
+	mouseX = p.width/2
+    mouseY = p.height/2
+    
   p.strokeCap(p.ROUND);
   p.strokeJoin(p.ROUND);
   p.noFill();
+
 
   _objectNum = 16;
   _nsRate = 0.001;
@@ -23,13 +41,49 @@ p.setup = function()  {
 
 p.draw = function() {
   p.clear();
-  p.blendMode(p.BURN);
+  p.blendMode(p.DIFFERENCE);
   p.background(p.CMYK, 0, 0, 0, 90);
+
+  WebglGraphics.shader(theShader)
+	theShader.setUniform('u_resolution',[p.width/1000,p.height/1000])
+	theShader.setUniform('u_time',p.millis()/10000)
+	theShader.setUniform('u_mouse',[mouseX/p.width,mouseY/p.height])
+	theShader.setUniform('tex0',drawingGraphics)
+	
+    drawingGraphics.clear(0,0,p.width,p.height)
+    
+    drawingGraphics.stroke(255)
+	drawingGraphics.strokeWeight(2)
+	drawingGraphics.push()
+	if (p.mouseIsPressed){
+		drawingGraphics.noFill()
+	}else{
+		drawingGraphics.fill(147,3,176)
+	}
+		
+		// drawingGraphics.noFill()
+	for(var i=00;i<800;i+=200){
+		drawingGraphics.ellipse(p.mouseX,p.mouseY,i)
+	}
+    drawingGraphics.pop()
+    
+    drawingGraphics.push()
+		
+
+		if (p.mouseIsPressed){
+			drawingGraphics.fill(255)
+		}else{
+			drawingGraphics.fill(0)
+		}
 
   for (let i = 0; i < _objectNum; i++) {
     _aryObject[i].update();
     _aryObject[i].draw();
   }
+
+  WebglGraphics.rect(-p.width/2,-p.height/2,p.width,p.height)
+	
+	p.image(WebglGraphics,0,0)
 }
 
 p.line = class{
